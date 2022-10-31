531f8069d68796753540f8798a2084e35f81748b
Don Blair <donblair@gmail.com>
2022-10-30 20:56:11 -0400

    update

---
+++ sender_two/sender_two.ino
@@ -0,0 +1,317 @@
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+#include <OneWire.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+
+/* for feather32u4 
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+*/
+
+typedef struct {
+  int           nodeId; //store this nodeId
+  float         batt; //uptime in ms
+  float         temp1;   //temperature maybe?
+  float         temp2;
+} Payload;
+Payload theData;
+
+
+// for feather m0  
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+
+/* for shield 
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 7
+*/
+
+/* Feather 32u4 w/wing
+#define RFM95_RST     11   // "A"
+#define RFM95_CS      10   // "B"
+#define RFM95_INT     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
+*/
+
+/* Feather m0 w/wing 
+#define RFM95_RST     11   // "A"
+#define RFM95_CS      10   // "B"
+#define RFM95_INT     6    // "D"
+*/
+
+#define VBATPIN A7
+   
+
+
+#if defined(ESP8266)
+  /* for ESP w/featherwing */ 
+  #define RFM95_CS  2    // "E"
+  #define RFM95_RST 16   // "D"
+  #define RFM95_INT 15   // "B"
+
+#elif defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)
+  // Feather M0 w/Radio
+  #define RFM95_CS      8
+  #define RFM95_INT     3
+  #define RFM95_RST     4
+
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2) || defined(ARDUINO_NRF52840_FEATHER) || defined(ARDUINO_NRF52840_FEATHER_SENSE)
+  #define RFM95_INT     9  // "A"
+  #define RFM95_CS      10  // "B"
+  #define RFM95_RST     11  // "C"
+  
+#elif defined(ESP32)  
+  /* ESP32 feather w/wing */
+  #define RFM95_RST     27   // "A"
+  #define RFM95_CS      33   // "B"
+  #define RFM95_INT     12   //  next to A
+
+#elif defined(ARDUINO_NRF52832_FEATHER)
+  /* nRF52832 feather w/wing */
+  #define RFM95_RST     7   // "A"
+  #define RFM95_CS      11   // "B"
+  #define RFM95_INT     31   // "C"
+  
+#elif defined(TEENSYDUINO)
+  /* Teensy 3.x w/wing */
+  #define RFM95_RST     9   // "A"
+  #define RFM95_CS      10   // "B"
+  #define RFM95_INT     4    // "C"
+#endif
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+OneWire  ds(12);  // on pin 10 (a 4.7K resistor is necessary)
+
+#define LED 13
+void setup() 
+{
+
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+
+  delay(100);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  digitalWrite(LED, LOW);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+const int num_sensors = 2; // number of temp sensors
+float temps[num_sensors];
+
+
+void loop()
+{
+
+for (int j = 0; j< num_sensors; j++) {
+
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[9];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+
+
+    Serial.print("sensor ");
+  Serial.print(j);
+  Serial.print("=");
+  Serial.println(celsius);
+
+if (j==0) {
+  theData.temp1=celsius;
+}
+if (j==1) {
+
+theData.temp2 = celsius;
+
+float measuredvbat = analogRead(VBATPIN);
+measuredvbat *= 2;    // we divided by 2, so multiply back
+measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+measuredvbat /= 1024; // convert to voltage
+
+theData.batt = measuredvbat;
+
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+  
+  char radiopacket[20] = "Hello World #      ";
+  itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  //rf95.send((uint8_t *)radiopacket, 20);
+  //rf95.send((const void*)(&theData), sizeof(theData))
+  rf95.send((uint8_t *)&theData, sizeof(theData));
+    digitalWrite(LED, HIGH);
+
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  rf95.waitPacketSent();
+      digitalWrite(LED, LOW);
+
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+
+}
+
+}
+}
